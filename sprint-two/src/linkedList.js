9115e216b8c6197590c2ae7369036d1220fd3f9f
veena-sridhar <v.sridhar91@gmail.com>
2016-04-21 11:40:26 -0700

    struggling with linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,20 +1,3 @@
-var LinkedList = function() {
-  var list = {};
-  list.head = null;
-  list.tail = null;
-
-  list.addToTail = function(value) {
-  };
-
-  list.removeHead = function() {
-  };
-
-  list.contains = function(target) {
-  };
-
-  return list;
-};
-
 var Node = function(value) {
   var node = {};
 
@@ -23,6 +6,34 @@ var Node = function(value) {
 
   return node;
 };
+
+var LinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+  
+  list.addToTail = function(value) {
+    
+    list.tail = {value: value};
+
+  };
+
+  list.removeHead = function() {
+    delete list.head;
+  };
+
+  list.contains = function(target) {
+    for(var key in this.node){
+      if(this.node.value === target ){
+        return true;
+      }
+    }
+    return false;
+  };
+
+  return list;
+};
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
