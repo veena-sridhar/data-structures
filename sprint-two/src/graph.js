293e61b1ce6753b737dc944421f34731d99e1f8f
veena-sridhar <v.sridhar91@gmail.com>
2016-04-22 11:25:07 -0700

    test commit for ESlint

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,31 +3,57 @@
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
+  var storage = [];
+  var edgeStorage = [];
+  this.storage = storage;
+  this.edgeStorage = edgeStorage;
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.storage.push(node);
+
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  for (var i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === node) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for (var i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === node) {
+      this.storage.splice(i, 1);
+    }
+  }
 };
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+    for (var i = 0; i < this.edgeStorage.length; i++) {
+      if (this.edgeStorage[i] == [fromNode, toNode]) {
+        return true;
+      }
+    }
+    return false;
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  //possibility 1: this.edgeStorage.push({fromNode: toNode});
+  //possibility 2: this.edgeStorage.push({from: fromNode, to: toNode})
+  this.edgeStorage.push([fromNode, toNode]);
 };
 
 // ------------------------
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -18,30 +18,32 @@ treeMethods.addChild = function(value) {
   
 };
 
-treeMethods.contains = function(target, currentNode, flag) {
+treeMethods.contains = function(target) {
   //current node that we're going to inspect. Starts with parent node;
   var currentNode = currentNode || this;
   //flag if we found something or not
   var flag = flag || false;
+  var helperFunction = function (currentNode) {
+    for (var i = 0; i < currentNode.children.length; i++) {
+    //loop through all children of current node
 
-  //loop through all children of current node
-  for (var i = 0; i < currentNode.children.length; i++) {
-
-    //base case: this level has the value
-    if (currentNode.children[i].value === target) {
-      flag = true;
-    }
-
-    //recursive case inspect children
-    if (currentNode.children[i].children.length > 0) {
-      var result = this.contains(target, currentNode.children[i], flag);
-      if (result === true) {
+      //base case: this level has the value
+      if (currentNode.children[i].value === target) {
         flag = true;
       }
-    }
 
-  }
-  
+      //recursive case inspect children
+      if (currentNode.children[i].children.length > 0) {
+        var result = helperFunction(currentNode.children[i]);
+        if (result === true) {
+          flag = true;
+        }
+      }
+
+    }
+    
+  };
+  helperFunction(currentNode);
   return flag;
 };
 
