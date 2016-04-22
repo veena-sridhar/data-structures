1d0b6410746774a9241e085c181a8f1f8c1fe9e7
veena-sridhar <v.sridhar91@gmail.com>
2016-04-21 21:25:52 -0700

    Last specification remaining on tree prompt.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-
+  
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,9 +11,38 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(); 
+  child.value = value;
+  // debugger;
+  this.children.push(child);
+  
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target, flag) {
+  // var flag = flag || false;
+  // for (var i = 0; i < this.children.length; i++) {
+  //   if (this.children[i].value === target) {
+  //     flag = true;
+  //   } else if (this.children[i].children.length > 0) {
+  //     this.children[i].contains(target, flag);
+  //   }
+  // }
+  // return flag;
+
+  while (currentNode.children.length > 0){
+    for (var i = 0; i < currentNode.children.length; i++) {
+      if (currentNode.value === target){
+        flag = true;
+      }
+      
+    }
+
+  }
+
+
+
+
+
 };
 
 
