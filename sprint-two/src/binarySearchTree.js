845de1efc1b831b32b70a840b7ba820493d1c254
veena-sridhar <v.sridhar91@gmail.com>
2016-04-22 16:41:16 -0700

    Working on binary search tree prompt.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,91 @@
 var BinarySearchTree = function(value) {
+
+  var obj = Object.create(searchTreeMethods);
+  obj.left = null;
+  obj.right = null;
+  obj.value = value;
+  // obj.storage = {};
+  // obj.storage.left = null;
+  // obj.storage.right = null;
+  return obj;
 };
 
+var findParent = function (tree, value) {
+  if (value > tree.value) {
+    //RECURSIVE CASEright is occupied
+    if (tree.right) {
+      return findParent(tree.right, value);
+    } else {
+    //BASE CASE right is free
+      return tree;        
+    }
+  }
+  if (value <= tree.value) {
+    //RECURSIVE CASE left is occupied
+    if (tree.left) {
+      return findParent(tree.left, value);
+    } else {
+    //BASE CASE left is free
+      return tree;        
+    }
+  }
 
+  return node;
+};
+
+searchTreeMethods = {
+
+  insert: function (value) {
+    //if it's an empty tree, just add it    
+    if (this.value === undefined) {
+      this.value = value;
+      return;
+    }
+    // find the parent node
+    var parentNode = findParent(this, value);
+
+    //create new Node
+    var newNode = {};
+    newNode.value = value;
+    newNode.left = null;
+    newNode.right = null;
+
+
+    // if it's bigger - assign to right
+    if (value > parentNode.value) {
+      parentNode.right = newNode;
+    } 
+    // if it's smaller - assign to left
+    if (value <= parentNode.value) {
+      parentNode.left = newNode;
+    } 
+
+
+  },
+
+  contains: function (value) {
+    var findValue = function (obj) {
+      if (value === obj.value) {
+        return true;
+      } else {
+        if (value > obj.value) {
+          return findValue(obj.right);
+        } else if (value <= obj.value) {
+          return findValue(obj.left);
+        }
+      }
+      findValue(obj);
+      return false;
+    };
+
+    
+  },
+
+  depthFirstLog: function (value) {
+
+  }
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
