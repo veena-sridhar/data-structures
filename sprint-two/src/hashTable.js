3c56d3253fcda32f3d9e22aca309e6a338a45407
veena-sridhar <v.sridhar91@gmail.com>
2016-04-22 15:05:09 -0700

    Completed hash table prompt.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,23 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.converter = getIndexBelowMaxForKey;
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this.converter(k, this._limit);
+  this._storage.set(index, v);
+
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this.converter(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this.converter(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
